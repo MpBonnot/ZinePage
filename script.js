3e071971fe491fcd71894502bffadf5b8a89a7fb
MpBonnot <bonnm737@newschool.edu>
2021-03-12 01:39:13 -0500

    changes

---
+++ script.js
@@ -0,0 +1,101 @@
+// define our first variable
+// when someone clicks on acorn icon, we want the background to turn red
+
+let acornIcon = document.querySelector(".acorn");
+
+// what happens when someone interacts with the acorn
+function turnRed() {
+    document.body.style.background = "rgb(224,64,26)";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+acornIcon.addEventListener("mouseover", turnRed);
+
+
+
+let coinIcon = document.querySelector(".coin");
+
+// what happens when someone interacts with the coin
+function turnGold() {
+    document.body.style.background = "#e89f05";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+coinIcon.addEventListener("mouseover", turnGold);
+
+
+let mothIcon = document.querySelector(".moth");
+
+// what happens when someone interacts with the coin
+function turnGreen() {
+    document.body.style.background = "#082e0f";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+mothIcon.addEventListener("mouseover", turnGreen);
+
+
+let mushroomIcon = document.querySelector(".mushroom");
+
+// what happens when someone interacts with the coin
+function turnBlue() {
+    document.body.style.background = "#0d8580";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+mushroomIcon.addEventListener("mouseover", turnBlue);
+
+
+let owlIcon = document.querySelector(".owl");
+
+// what happens when someone interacts with the coin
+function turnGrey() {
+    document.body.style.background = "#3e3936";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+owlIcon.addEventListener("mouseover", turnGrey);
+
+
+
+let parrotIcon = document.querySelector(".parrot");
+
+// what happens when someone interacts with the coin
+function turnPink() {
+    document.body.style.background = "#f6bdd6";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+parrotIcon.addEventListener("mouseover", turnPink);
+
+
+let pearIcon = document.querySelector(".pear");
+
+// what happens when someone interacts with the coin
+function turnlimeGreen() {
+    document.body.style.background = "#b3d430";
+
+}
+
+//add an event listener
+// when someone clicks on the red circle, change the background red
+
+pearIcon.addEventListener("mouseover", turnlimeGreen);
